a33486f5dae8839fc21dd173542a90eaa5180cc5
fredric89 <139386397+fredric89@users.noreply.github.com>
2025-06-12 14:13:55 +0800

    Update appMain.py

--- appMain.py
+++ appMain.py
@@ -15,7 +15,7 @@ if "page" not in st.session_state:
 if st.session_state.page in ["home", "about"]:
     st.set_page_config(page_title="Voice Pitch Detector", layout="centered")
 
-    import urllib
+import urllib
 share_url = urllib.parse.quote_plus(st.get_option("server.address", fallback="http://localhost:8501"))
 
     st.markdown(f"""
